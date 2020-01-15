142e6fadafbb08917dc483ba176e3e6f3257909e
Fernando JS Silva <development.ferjssilva@gmail.com>
2020-01-15 11:02:01 -0300

    feature: create a list and edit itens

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const MOCKED_HEROES: Hero[] = [
+  { id: 13, name: 'Zero' },
+  { id: 14, name: 'Cinnamon' },
+  { id: 15, name: 'Top' },
+  { id: 16, name: 'Fox' },
+  { id: 17, name: 'Lorfle' },
+  { id: 18, name: 'Carlton Dark' },
+  { id: 19, name: 'Parzival' },
+  { id: 20, name: 'Art3mis' },
+  { id: 21, name: 'Aech' },
+  { id: 22, name: 'Daito' },
+  { id: 23, name: 'Sho' },
+];
